68f9c46bd390e3b99023d4026f9af6675107a4cb
Bryan.D Pro <74014416+bryandelaitre@users.noreply.github.com>
2024-08-09 09:31:49 +0200

    Repo init

---
+++ .changeset/thick-rockets-allow.md
@@ -0,0 +1,5 @@
+---
+'@wanderland-agency/wd-swiper': patch
+---
+
+Repo init
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,5 @@ import type { Webflow as WebflowType } from '@finsweet/ts-utils'
 export const Webflow = window.Webflow as WebflowType
 
 Webflow.push(() => {
-  //Write your code here
-
-  console.log('Hello World')
+    console.log('Hello World')
 })
